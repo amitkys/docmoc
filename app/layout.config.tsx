0f5a466529b8d40c4209378f321be25ed9054f64
KYs <amitkys59@gmail.com>
2025-04-03 00:47:10 +0530

    change title of the site

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -19,13 +19,13 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        DocMoc
       </>
     ),
   },
   links: [
     {
-      text: 'Documentation',
+      text: 'Doc',
       url: '/docs',
       active: 'nested-url',
     },
