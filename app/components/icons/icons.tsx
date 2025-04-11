f5506d1669e3d219f78d1d2a508cf9225a52f98a
KYs <amitkys59@gmail.com>
2025-04-11 18:14:42 +0530

    added card for project

---
+++ app/components/icons/icons.tsx
@@ -0,0 +1,9 @@
+import { PiNumberSquareOneFill } from "react-icons/pi";
+import { PiNumberSquareTwoFill } from "react-icons/pi";
+import { PiNumberSquareThreeFill } from "react-icons/pi";
+
+export {
+  PiNumberSquareOneFill,
+  PiNumberSquareTwoFill,
+  PiNumberSquareThreeFill
+}
