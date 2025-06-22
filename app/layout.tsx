2b85be927f4a896b93300e76a18bd46313c70946
KYs <amitkys59@gmail.com>
2025-06-22 20:26:08 +0530

    geist font package is used instead of from google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,15 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import type { ReactNode } from 'react';
-import { Geist, Geist_Mono } from 'next/font/google';
 
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
-const geist = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const mono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en"
-      className={`${geist.className} ${mono.variable} antialiased`}
+      className={`${GeistSans.className} ${GeistMono.variable} antialiased`}
       suppressHydrationWarning>
       <body className="flex flex-col min-h-screen bg-background text-foreground">
         <RootProvider>{children}</RootProvider>
